3ab07c37d23f75c86485ea50861ef77f8270abe4
Xenia Levchenko <variellka@gmail.com>
2024-05-24 23:26:25 +0300

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,27 @@ const nextConfig = {
     AUTH_KEY: process.env.AUTH_KEY,
     URL_FOR_MOVIE_DETAILS: process.env.URL_FOR_MOVIE_DETAILS
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Authorization',
+            value: process.env.AUTH_KEY,
+          },
+          {
+            key: 'accept',
+            value: 'application/json',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      }
+    ]
+  },
   async redirects() {
     return [
       {
